edd5af50e078b2f2570bbba2ccfdb5e8c0d024e6
divyavittal <divya.vittal@idt.com>
2017-03-31 13:12:55 -0700

    Add files via upload

---
+++ 4loop.py
@@ -0,0 +1,12 @@
+a1 = float(input("ener forst num:"))
+a2 = float(input("enter sec num:"))
+a3 = float(input("enter third num:"))
+if(a1 > a2) and (a1 > a3):
+    print "largest is ",a1
+elif(a2 >a1) and (a2 > a3):
+    print"largest is",a2
+else:
+    print "largest is",a3
+
+    
+
