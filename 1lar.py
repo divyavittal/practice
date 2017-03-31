462fb0276f4767604078e36e85f6fcd256fecdd5
divyavittal <divya.vittal@idt.com>
2017-03-31 13:15:27 -0700

    Add files via upload

---
+++ 1lar.py
@@ -0,0 +1,17 @@
+print"largest of nos"
+
+print'largest of nos'
+
+print("largest of nos")
+
+print('largest of nos')
+
+x = [1,8,6,3]
+print("list elements are ", x)
+x.sort()
+x=map(int,x)
+
+print x
+
+
+        
